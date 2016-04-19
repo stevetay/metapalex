8a3ae93bce12edff021f216fc7bb2ceceec337e1
Steve Tay <stevetay@appsdesignstudio.com>
2016-04-19 23:35:41 +0800

    sendgrip api

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,12 +15,17 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+require("../sendgrid-php/sendgrid-php.php");
+
+$sendgrid = new SendGrid('stevetay', 'stay146081');
+$email    = new SendGrid\Email();
+
+$email->addTo("stevetay23@gmail.com")
+      ->setFrom("newcontact@metapalex.com")
+      ->setSubject("Website Contact Form:  $name")
+      ->setHtml("You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message");
+
+$sendgrid->send($email);
+
 return true;			
 ?>
