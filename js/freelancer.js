99da243f1d0ede4bdbb7e4af06de3657f85b46d5
Steve Tay <stevetay@appsdesignstudio.com>
2016-06-20 02:42:22 +0800

    update server

--- js/freelancer.js
+++ js/freelancer.js
@@ -3,8 +3,36 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-
+ 
+ $(function(){
+ 	
+ 	var $window = $(window);		//Window object
+ 	
+ 	var scrollTime = 0.3;			//Scroll time
+ 	var scrollDistance = 50;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+ 	
+ 	$window.on("mousewheel DOMMouseScroll", function(event){
+ 		
+ 		event.preventDefault();	
+ 										
+ 		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+ 		var scrollTop = $window.scrollTop();
+ 		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+ 			
+ 		TweenMax.to($window, scrollTime, {
+ 			scrollTo : { y: finalScroll, autoKill:true },
+ 				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+ 				autoKill: true,
+ 				overwrite: 5							
+ 			});
+ 					
+ 	});
+ 	
+ });
+ 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
@@ -34,30 +62,4 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
-});
-
-$(function(){
-	
-	var $window = $(window);		//Window object
-	
-	var scrollTime = 0.3;			//Scroll time
-	var scrollDistance = 50;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
-	
-	$window.on("mousewheel DOMMouseScroll", function(event){
-		
-		event.preventDefault();	
-										
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-			
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5							
-			});
-					
-	});
-	
 });
